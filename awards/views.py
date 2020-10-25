c67ac8d8f01cf190c26063c3ed29c6d2a2bce59e
Balvine <balvy075@gmail.com>
2020-10-25 19:25:28 +0300

    adde d login

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import  Profile
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    projects = Project.objects.all().order_by('-posted_on')
+    form = DesignForm()
+    form = UsabilityForm()
+    form = ContentForm()
+    return render(request, 'index.html', locals())
+
