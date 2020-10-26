61bc54f07d7d399e6ffad167ccc85d43e78601d8
Balvine <balvy075@gmail.com>
2020-10-26 10:34:07 +0300

    updated urls file

--- awards/urls.py
+++ awards/urls.py
@@ -1,8 +1,10 @@
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url('^$', views.index, name='landing')
+     url(r'^upload/$', views.new_project, name='newPro'),
+    url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
 ]
