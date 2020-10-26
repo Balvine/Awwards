0e16454281bf674923237ded02cf0d1df4550986
Balvine <balvy075@gmail.com>
2020-10-26 08:30:57 +0300

    added a decorater

--- awards/views.py
+++ awards/views.py
@@ -12,3 +12,22 @@ def index(request):
     form = ContentForm()
     return render(request, 'index.html', locals())
 
+
+# @login_required(login_url='/accounts/login/')
+def new_project(request):
+    """
+    Function that enables one to upload projects
+    """
+    profile = Profile.objects.all()
+    for profile in profile:
+        if request.method == 'POST':
+            form = ProjectForm(request.POST, request.FILES)
+            if form.is_valid():
+                pro = form.save(commit=False)
+                pro.profile = profile
+                pro.user = request.user
+                pro.save()
+            return redirect('landing')
+        else:
+            form = ProjectForm()
+    return render(request, 'new_pro.html', {"form": form})
