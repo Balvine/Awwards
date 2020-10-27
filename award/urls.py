dc6c058fcffd70a7f45ce34baa41e437b0b328a0
Balvine <balvy075@gmail.com>
2020-10-27 12:32:55 +0300

    made changes to settings file

--- award/urls.py
+++ award/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('awards.urls')),
     # url(r'^logout/$', views.logout, {"next_page": '/'}),
-    # url(r'^accounts/', include('registration.backends.simple.urls')),
-    # url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
 ]
